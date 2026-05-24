383d6d12c362ef1410352ae9ad5bdc000b2aa1d6
ogoregen <37447279+ogoregen@users.noreply.github.com>
2026-05-25 00:27:41 +0300

    Fix indentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 
-import markdoc from '@astrojs/markdoc';
 import { defineConfig } from 'astro/config';
+import markdoc from '@astrojs/markdoc';
 import config from './site/config.json';
 
 export default defineConfig({
-  site: config.url,
-  base: config.subdomain,
-  markdown: {
-    shikiConfig: {
-      theme: 'one-light',
-    },
+	site: config.url,
+	base: config.subdomain,
+	markdown: {
+		shikiConfig: {
+			theme: 'one-light',
+		},
 	},
-  integrations: [markdoc()],
+	integrations: [markdoc()],
 });
